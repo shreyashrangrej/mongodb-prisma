6c737c20ac42250684251132ded02b1778857e4d
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-01-18 11:33:49 +0530

    installed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+require('dotenv').config()
 import express from 'express'
 import router from './routes/user'
 const app = express()
@@ -6,7 +7,7 @@ app.use('/api', router)
 app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
-const port = 3000
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`)
+    console.log(`Server running on Port: ${port}`)
 })
